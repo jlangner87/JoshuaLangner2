4356c1e7744c9c6775293764fabfb9ff201ba428
jlangner87 <josh.langner@gmail.com>
2022-11-30 12:07:00 -0600

    footer and additional styling to UI

---
+++ client/src/components/footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer_content">
+        <p className="footer_text">Web Development by Joshua Langner</p>
+        <p className="footer_text">©2022 All rights reserved</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
