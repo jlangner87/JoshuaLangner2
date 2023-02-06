e23810904c4ae3d727b930c4dc65ccfa9a9ca4e6
jlangner87 <josh.langner@gmail.com>
2023-02-06 15:37:40 -0600

    mobile styling for blog & add additional link card

--- client/src/pages/splash.jsx
+++ client/src/pages/splash.jsx
@@ -42,6 +42,16 @@ function Splash() {
           <p className='blog_meta_description'>React Router's Hashlink can help you shave time off your development process. In this article, I cover what it is, when you should use it, and provide a step-by-step walk-through for implementing in your React application.</p>
           <p className='blog_meta_description'> Hosted at: <a href='https://medium.com/@josh-langner'>medium.com/@josh-langner</a></p>
         </div>
+        <div className='blog_container'>
+          <a href='https://medium.com/@josh-langner/time-saving-react-components-slideshow-8adbaeb43713'>
+            <img src="https://miro.medium.com/v2/resize:fit:1384/format:webp/1*0jDoinoSBpAuG_E-rTZC3A.png" alt="Title: Time saving tips for react: hashlink" className='blog_image'/>
+          </a>
+          <a href='https://medium.com/@josh-langner/time-saving-react-components-slideshow-8adbaeb43713'>
+            <h2 className='blog_title'>Time Saving Tips for React | Slideshow</h2>
+          </a>
+          <p className='blog_meta_description'>React Image Slideshow saves hours of tedious styling and conserves coutless lines of code. In this article, I cover what it is, when you should use it, and provide a step-by-step walk-through for implementing in your React application.</p>
+          <p className='blog_meta_description'> Hosted at: <a href='https://medium.com/@josh-langner'>medium.com/@josh-langner</a></p>
+        </div>
 
       </div>
 
