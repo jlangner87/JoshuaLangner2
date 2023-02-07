c9b4d4947d8d1123a57e25438396f48824dad408
jlangner87 <josh.langner@gmail.com>
2023-02-07 10:54:32 -0600

    reconfigre blog component

---
+++ client/src/components/blogs.jsx
@@ -0,0 +1,32 @@
+function Blogs() {
+  return (
+    <div className="content_creation">
+      <h1 className='splash_header'>Blogs & Articles by Joshua Langer</h1>
+      <div className='blogs'>
+        <div className='blog_container'>
+          <a href='https://medium.com/@josh-langner/time-saving-react-components-hashlink-887e76fc4e05'>
+            <img src="https://miro.medium.com/v2/resize:fit:1400/format:webp/1*PZ4S2wd-Ia6poz390Rp6cA.png" alt="Title: Time saving tips for react: hashlink" className='blog_image'/>
+          </a>
+          <a href='https://medium.com/@josh-langner/time-saving-react-components-hashlink-887e76fc4e05'>
+            <h2 className='blog_title'>Time Saving Tips for React | HashLink</h2>
+          </a>
+          <p className='blog_meta_description'>React Router's Hashlink can help you shave time off your development process. In this article, I cover what it is, when you should use it, and provide a step-by-step walk-through for implementing in your React application.</p>
+          <p className='blog_meta_description'> Hosted at: <a href='https://medium.com/@josh-langner'>medium.com/@josh-langner</a><br/>Created: Feb. 06, 2023</p>
+        </div>
+        <div className='blog_container'>
+          <a href='https://medium.com/@josh-langner/time-saving-react-components-slideshow-8adbaeb43713'>
+            <img src="https://miro.medium.com/v2/resize:fit:1384/format:webp/1*0jDoinoSBpAuG_E-rTZC3A.png" alt="Title: Time saving tips for react: hashlink" className='blog_image'/>
+          </a>
+          <a href='https://medium.com/@josh-langner/time-saving-react-components-slideshow-8adbaeb43713'>
+            <h2 className='blog_title'>Time Saving Tips for React | Slideshow</h2>
+          </a>
+          <p className='blog_meta_description'>React Image Slideshow saves hours of tedious styling and conserves coutless lines of code. In this article, I cover what it is, when you should use it, and provide a step-by-step walk-through for implementing in your React application.</p>
+          <p className='blog_meta_description'> Hosted at: <a href='https://medium.com/@josh-langner'>medium.com/@josh-langner</a><br/>Created: Feb. 06, 2023</p>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Blogs
